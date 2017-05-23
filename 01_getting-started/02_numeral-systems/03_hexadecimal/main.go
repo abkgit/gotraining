a52e70c56e8b494e8668f0540d33dc085216094e
abkgit <elite.abhishek@gmail.com>
2017-05-23 21:01:23 +0530

    01_getting-started Rearrangement

--- 01_getting-started/02_numeral-systems/03_hexadecimal/main.go
+++ 01_getting-started/02_numeral-systems/03_hexadecimal/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Printf("%d - %b - %x\n", 42, 42, 42)
-	// fmt.Printf("%d - %b - %#x\n", 42, 42, 42) // #%x gives the 0x numeral notation
+	// fmt.Printf("%d - %b - %#x\n", 42, 42, 42) // %#x gives the 0x numeral notation
 	// fmt.Printf("%d - %b - %#X\n", 42, 42, 42)   // capital %X for capital hex digits
 	// fmt.Printf("%d \t %b \t %#X\n", 42, 42, 42) // tab escape char, \t
 }
