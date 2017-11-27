e310ba9fabae8ff6a424fd92abe3ffad5e71fdd3
abkgit <elite.abhishek@gmail.com>
2017-11-27 22:12:14 +0530

    22_go-routines.6(fmt)

--- 21_interfaces/05_conversion-vs-assertion/02_assertion/01_non-interface-error_invalid-code/main.go
+++ 21_interfaces/05_conversion-vs-assertion/02_assertion/01_non-interface-error_invalid-code/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	name := "Sydney"
-	str, ok := name.(string)	// Trying to use assertion without an interface
+	str, ok := name.(string) // Trying to use assertion without an interface
 	if ok {
 		fmt.Printf("%q\n", str)
 	} else {
